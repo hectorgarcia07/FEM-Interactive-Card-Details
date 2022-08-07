51dff3b58dce30b8800a6c953458ee31310d340f
hectorgarcia07 <csharp12321@gmail.com>
2022-08-06 21:26:52 -0500

    Refactor form

--- src/components/CreditInfoForm.tsx
+++ src/components/CreditInfoForm.tsx
@@ -1,5 +1,6 @@
 import { Button, Grid } from '@mui/material';
 import { Formik, Field, Form, ErrorMessage } from 'formik';
+import { MyTextInput } from './CustomInputs'
 import { validationSchema, initialValues } from '../Yup/formValidation'
 
 const CreditInfoForm = () => {
@@ -17,6 +18,49 @@ const CreditInfoForm = () => {
       <Form>
         <Grid container spacing={2}>
           <Grid item xs={12}>
+            <MyTextInput
+              label="CARHOLDER NAME"
+              ariaDescribe="Enter cardholder name"
+              name="name"
+              type="text"
+              placeholder="e.g. Jane Appleseed"
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <label htmlFor="cardNumber">CARD NUMBER</label>
+            <Field name="cardNumber" type="text" />
+            <ErrorMessage name="cardNumber" />
+          </Grid>
+          <Grid item xs={3}>
+            <label htmlFor="month">EXP. DATE</label>
+            <Field name="month" type="text" />
+            <ErrorMessage name="month" />
+          </Grid>
+          <Grid item xs={3}>
+            <label htmlFor="year">(MM/YY)</label>
+            <Field name="year" type="text" />
+            <ErrorMessage name="year" />
+          </Grid>
+          <Grid item xs={6}>
+            <label htmlFor="cvc">CVC</label>
+            <Field name="cvc" type="text" />
+            <ErrorMessage name="cvc" />
+          </Grid>
+          <Grid item xs={12}>
+            <Button color="primary" variant="contained" fullWidth type="submit">
+              Submit
+            </Button>
+          </Grid>
+        </Grid>
+      </Form>
+    </Formik>
+  )
+}
+
+export default CreditInfoForm
+
+/*
+<Grid item xs={12}>
             <label htmlFor="name">CARHOLDER NAME</label>
             <Field name="name" type="text" />
             <ErrorMessage name="name" />
@@ -46,10 +90,4 @@ const CreditInfoForm = () => {
               Submit
             </Button>
           </Grid>
-        </Grid>
-      </Form>
-    </Formik>
-  )
-}
-
-export default CreditInfoForm
+*/
--- src/components/CustomInputs.tsx
+++ src/components/CustomInputs.tsx
@@ -1,31 +1,41 @@
-import { FormControl, InputLabel, Input, FormHelperText } from '@mui/material';
+import { FormControl, InputLabel, FormHelperText, OutlinedInput } from '@mui/material';
 import { useField } from 'formik';
 
-const MyTextInput = ({ label, ...props }) => {
+interface myTextProps {
+  label: string;
+  ariaDescribe: string;
+  name: string;
+  type: string;
+  placeholder: string;
+}
 
+export const MyTextInput = ({ label, ariaDescribe, ...props }:myTextProps) => {
   // useField() returns [formik.getFieldProps(), formik.getFieldMeta()]
   // which we can spread on <input>. We can use field meta to show an error
   // message if the field is invalid and it has been touched (i.e. visited)
-
   const [field, meta] = useField(props);
+  console.log("STUFF ", field, meta)
   return (
-    <FormControl>
+    <FormControl variant='outlined'>
       <InputLabel htmlFor={props.name}>{label}</InputLabel>
-      <Input id={props.name} aria-describedby="my-helper-text" />
-      <FormHelperText id="my-helper-text">We'll never share your email.</FormHelperText>
+      <OutlinedInput 
+        id={props.name} 
+        aria-describedby={ariaDescribe} 
+        {...field} 
+        {...props}
+        type="search" 
+      />
+      <FormHelperText>{meta.error}</FormHelperText>
     </FormControl>
-    <>
-      <label htmlFor={props.id || props.name}>{label}</label>
-      <input className="text-input" {...field} {...props} />
-      {meta.touched && meta.error ? (
-        <div className="error">{meta.error}</div>
-      ) : null}
-    </>
   );
 };
 
-<FormControl>
-  <InputLabel htmlFor="my-input">Email address</InputLabel>
-  <Input id="my-input" aria-describedby="my-helper-text" />
-  <FormHelperText id="my-helper-text">We'll never share your email.</FormHelperText>
-</FormControl>
+/*
+<>
+  <label htmlFor={props.id || props.name}>{label}</label>
+  <input className="text-input" {...field} {...props} />
+    {meta.touched && meta.error ? (
+      <div className="error">{meta.error}</div>
+    ) : null}
+    </>
+*/
